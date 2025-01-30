bd2e9a913c62c84f2ef360e8bd2abd872e4b4edb
Yair-Bataglia <batagliayair@gmail.com>
2025-01-30 14:43:29 -0300

    Commit de pequeños arreglos

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-console.log("Proyecto limpio y listo para empezar 🚀");
+import { loadPage } from './scripts/spa';
+
+function navigate(page: string): void {
+    history.pushState({ page }, "", `#${page}`);
+    loadPage(page);
+}
+
+window.addEventListener("popstate", (event: PopStateEvent): void => {
+    if (event.state && event.state.page) {
+        loadPage(event.state.page as string);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", (): void => {
+    const page: string = location.hash.replace("#", "") || "home";
+    loadPage(page);
+
+    // Seleccionar todos los enlaces y añadirles el manejador de eventos
+    const navLinks = document.querySelectorAll('.nav-links a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevenir la recarga completa de la página
+            const page = link.getAttribute('href')?.replace('#', '') || '';
+            navigate(page);
+        });
+    });
+});
